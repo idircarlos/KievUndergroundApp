62bc126da34010e98fca1c1c94ae09f253887d62
Idir <idircarlos@gmail.com>
2021-11-06 17:22:00 +0100

    Database y Lineas actualizadas

--- app/database.py
+++ app/database.py
@@ -20,7 +20,57 @@ stop_125 = stop(125, 50.45595887549448, 30.612968983504803)
 stop_126 = stop(126, 50.45989135620235, 30.63029161718559)
 stop_127 = stop(127, 50.46465878887907, 30.645564379174083)
 
+stop_110.add_connection(stop_111, 1.4)
 
+stop_111.add_connection(stop_110, 1.4)
+stop_111.add_connection(stop_112, 1.9)
+
+stop_112.add_connection(stop_111, 1.9)
+stop_112.add_connection(stop_113, 1.1)
+
+stop_113.add_connection(stop_112, 1.1)
+stop_113.add_connection(stop_114, 1.1)
+
+stop_114.add_connection(stop_113, 1.1)
+stop_114.add_connection(stop_115, 2.1)
+
+stop_115.add_connection(stop_114, 2.1)
+stop_115.add_connection(stop_116, 1.3)
+
+stop_116.add_connection(stop_115, 1.3)
+stop_116.add_connection(stop_117, 1.9)
+
+stop_117.add_connection(stop_116, 1.9)
+stop_117.add_connection(stop_118, 1.3)
+
+stop_118.add_connection(stop_117, 1.3)
+stop_118.add_connection(stop_119, 0.7)
+
+stop_119.add_connection(stop_118, 0.7)
+stop_119.add_connection(stop_120, 0.68)
+
+stop_120.add_connection(stop_119, 0.68)
+stop_120.add_connection(stop_121, 1.6)
+
+stop_121.add_connection(stop_120, 1.6)
+stop_121.add_connection(stop_122, 0.98)
+
+stop_122.add_connection(stop_121, 0.98)
+stop_122.add_connection(stop_123, 1.4)
+
+stop_123.add_connection(stop_122, 1.4)
+stop_123.add_connection(stop_124, 1.6)
+
+stop_124.add_connection(stop_123, 1.6)
+stop_124.add_connection(stop_125, 1.2)
+
+stop_125.add_connection(stop_124, 1.2)
+stop_125.add_connection(stop_126, 1.4)
+
+stop_126.add_connection(stop_125, 1.4)
+stop_126.add_connection(stop_127, 1.3)
+
+stop_127.add_connection(stop_126, 1.3)
 
 
 
@@ -44,6 +94,60 @@ stop_225 = stop(225, 50.38247519076805, 30.477554159536847)
 stop_226 = stop(226, 50.37661806622592, 30.468823310597116)
 stop_227 = stop(227, 50.36709545601481, 30.45415814797518)
 
+stop_210.add_connection(stop_211, 1.2)
+
+stop_211.add_connection(stop_210, 1.2)
+stop_211.add_connection(stop_212, 1.2)
+
+stop_212.add_connection(stop_211, 1.2)
+stop_212.add_connection(stop_213, 1.7)
+
+stop_213.add_connection(stop_212, 1.7)
+stop_213.add_connection(stop_214, 1.4)
+
+stop_214.add_connection(stop_213, 1.4)
+stop_214.add_connection(stop_215, 1.2)
+
+stop_215.add_connection(stop_214, 1.2)
+stop_215.add_connection(stop_216, 1.0)
+
+stop_216.add_connection(stop_215, 1.0)
+stop_216.add_connection(stop_217, 1.3)
+
+stop_217.add_connection(stop_216, 1.3)
+stop_217.add_connection(stop_218, 0.988)
+
+
+stop_218.add_connection(stop_217, 0.988)
+stop_218.add_connection(stop_219, 1.1)
+
+
+stop_219.add_connection(stop_218, 1.1)
+stop_219.add_connection(stop_220, 1.2)
+
+stop_220.add_connection(stop_219, 1.2)
+stop_220.add_connection(stop_221, 0.836)
+
+stop_221.add_connection(stop_220, 0.836)
+stop_221.add_connection(stop_222, 1.2)
+
+stop_222.add_connection(stop_221, 1.2)
+stop_222.add_connection(stop_223, 1.1)
+
+stop_223.add_connection(stop_222, 1.1)
+stop_223.add_connection(stop_224, 2.9)
+
+stop_224.add_connection(stop_223, 2.9)
+stop_224.add_connection(stop_225, 1.5)
+
+stop_225.add_connection(stop_224, 1.5)
+stop_225.add_connection(stop_226, 0.924)
+
+stop_226.add_connection(stop_225, 0.924)
+stop_226.add_connection(stop_227, 1.48)
+
+stop_227.add_connection(stop_226, 1.48)
+
 
 
 #LINEA 3
@@ -63,3 +167,61 @@ stop_324 = stop(324, 50.400847047155395, 30.65205731205759)
 stop_325 = stop(325, 50.402951109703366, 30.66691051948101)
 stop_326 = stop(326, 50.403465565753365, 30.684370370647205)
 stop_327 = stop(327, 50.40954636743697, 30.69623245641607)
+
+stop_310.add_connection(stop_311 ,1.5)
+
+stop_311.add_connection(stop_310 ,1.5)
+stop_311.add_connection(stop_312 ,2.7)
+
+stop_312.add_connection(stop_311 ,2.7)
+stop_312.add_connection(stop_314 ,3.1)
+
+stop_314.add_connection(stop_312 ,3.1)
+stop_314.add_connection(stop_315 ,0.757)
+
+stop_315.add_connection(stop_314, 0.757)
+stop_315.add_connection(stop_316, 1.1)
+
+stop_316.add_connection(stop_315, 1.1)
+stop_316.add_connection(stop_317, 1.3)
+
+stop_317.add_connection(stop_316, 1.3)
+stop_317.add_connection(stop_318, 1.1)
+
+stop_318.add_connection(stop_317, 1.1)
+stop_318.add_connection(stop_319, 1.9)
+
+stop_319.add_connection(stop_318, 1.9)
+stop_319.add_connection(stop_321, 3.4)
+
+stop_321.add_connection(stop_319, 3.4)
+stop_321.add_connection(stop_322, 0.795)
+
+stop_322.add_connection(stop_321, 0.795)
+stop_322.add_connection(stop_323, 1.4)
+
+stop_323.add_connection(stop_322, 1.4)
+stop_323.add_connection(stop_324, 1.3)
+
+stop_324.add_connection(stop_323, 1.3)
+stop_324.add_connection(stop_325, 1.1)
+
+stop_325.add_connection(stop_324, 1.1)
+stop_325.add_connection(stop_326, 1.2)
+
+stop_326.add_connection(stop_325, 1.2)
+stop_326.add_connection(stop_327, 2.3)
+
+stop_327.add_connection(stop_326, 2.3)
+
+#Transbordos linea 1
+stop_119.add_connection(stop_314, 0.601)
+stop_120.add_connection(stop_217, 0.601)
+
+#Transbordos linea 2
+stop_217.add_connection(stop_120, 0.601)
+stop_218.add_connection(stop_315, 1.2)
+
+#Transbordos linea 3
+stop_314.add_connection(stop_119, 0.601)
+stop_315.add_connection(stop_218, 1.2)
